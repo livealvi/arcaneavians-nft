fd5bc568dcd3ac0af247714c88e2b37ea8f2532a
Ashik Rahman <ashrhmn99@gmail.com>
2022-01-14 02:23:10 +0600

    Fix array keys

--- src/components/NFT/NFT.js
+++ src/components/NFT/NFT.js
@@ -4,9 +4,9 @@ import "./NFT.css";
 const NFT = () => {
   return (
     <div className="nft">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((fileNo) => (
+      {[...Array(15).keys()].map((fileNo) => (
         <div className="nft__img" key={fileNo}>
-          <img src={`/NFT/NFT_${fileNo}.png`} alt=""></img>
+          <img src={`/NFT/NFT_${fileNo+1}.png`} alt=""></img>
         </div>
       ))}
     </div>
